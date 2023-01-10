b4b7b9a7c0c378e6f8279439d388dfe6f3615c7b
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2023-01-10 10:09:58 +0000

    Added image generation endpoint

--- app/app.py
+++ app/app.py
@@ -22,3 +22,18 @@ def index():
     return render_template("index.html", result=result)
 
 
+@app.route("/image", methods=("GET", "POST"))
+def image():
+    if request.method == "POST":
+        image = request.form["image"]
+        response = openai.Image.create(
+            prompt=image,
+            n=1,
+            size="1024x1024"
+        )
+        return redirect(url_for("image", result=response['data'][0]['url']))
+
+    result = request.args.get("result")
+    return render_template("image.html", result=result)
+
+
--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,4 +3,5 @@ apt-get update
 apt-get install -y python3 python3-pip
 cd /home/app
 pip install -r requirements.txt
+pip install -U openai
 flask run --host=0.0.0.0
